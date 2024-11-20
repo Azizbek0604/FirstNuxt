2964c0fc03b4df696fd9a931ced1455956760366
Azizbek0406 <azizbekcoder04@gmail.com>
2024-11-20 14:18:19 +0500

    first Nuxt

---
+++ nuxt/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores architecto iusto ut, modi eveniet voluptates ratione! Illum, error labore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores architecto iusto ut, modi eveniet voluptates ratione! Illum, error labore.</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    const {data} = await useFetch('/api/ninja')
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores architecto iusto ut, modi eveniet voluptates ratione! Illum, error labore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores architecto iusto ut, modi eveniet voluptates ratione! Illum, error labore.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
